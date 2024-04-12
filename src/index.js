80e661866ed3c930a620e886ed9fdcb166df6fdb
Abdusamig <abdusamigbaxodirov12@gmail.com>
2024-04-12 17:57:44 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<>
+<h1>hellon world</h1>
+<h1>hellon world</h1>
+<h1>hellon world</h1>
+<h1>hellon world</h1>
+<p>salomlar</p>
+</>
+);
+
+
